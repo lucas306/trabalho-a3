2be37a909d892cf237b2589cdba2b2e82cb9f66c
Luka_Petê <67662386+lucas306@users.noreply.github.com>
2022-10-19 16:33:41 -0300

    Update index.js
    andamento da construção da chamada das senhas

--- chamada-de-senha/index.js
+++ chamada-de-senha/index.js
@@ -2,14 +2,44 @@ const express = require('express')
 const app = express();
 const axios = require('axios');
 app.use(express.json());
+
+const {v4 : uuidv4} = require('uuid');
 const port = 5000;
+const chamadaDeSenhas = {};
 
 app.get('', async (req, res) => {
-
+    res.send(observacoesPorLembreteId[req.params.id] || []);
 });
 
-app.post('', async (req, res) => {
+app.post('/senhas/:id/', async (req, res) => {
+    const idObs = uuidv4();
+    const { texto } = req.body;
 
+    const chamadaDeSenha = chamadaDeSenhas[req.params.id] || [];
+    chamadaDeSenha.push({id: idObs, texto});
+    chamadaDeSenhas[req.params.id] = chamadaDeSenha;
+
+    axios.post('http://localhost:10000/eventos', {
+        tipo: "SenhaChamada",
+        dados:{
+            id: idObs,
+            texto,
+            lembreteId: req.params.id
+        }
+    });
+
+    res.status(200).send(chamadaDeSenhas)
+});
+
+app.post('/eventos', (req, res) => {
+    try{
+      funcoes[req.body.tipo](req.body.dados);  
+    }
+    catch (ex){
+      console.log(ex)
+      console.log(req.body)
+    }
+    res.status(200).send({msg: 'ok'})
 });
 
 app.listen(port, () =>{
